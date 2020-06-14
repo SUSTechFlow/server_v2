f522af2c53f66af1774174c9b925537a0bf694b4
john <799433746@qq.com>
2020-06-14 17:44:28 +0800

    test transfer

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use web_v2::database::Database;
 use futures::executor::block_on;
 
 fn main() {
+    print!("Hello, world");
     print!("{:?}", block_on(block_on(Database::new(None)).unwrap().connect()));
 }
